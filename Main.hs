2a03eb561ed0824f9c75102d313e4ce72153f995
Ryan Newton <rrnewton@gmail.com>
2012-11-20 14:53:11 -0500

    Cleanup pass

--- Main.hs
+++ Main.hs
@@ -7,30 +7,20 @@ import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Map as M
 import qualified Data.Set as S
 import           Control.Monad
-import           Control.Concurrent
--- Wow, I actually couldn't figure out how to open a file (and get a
--- HANDLE) so that I could then use getFileAttributes under
--- System.Win32.  Giving up because I think I can just use the
--- OS-independent System.Directory
--- import           System.FilePath
-import           System.Environment
-import           System.Console.GetOpt
-import           System.Exit
-import           Test.HUnit (runTestTT)
+import           Control.Concurrent    (Chan, readChan, ThreadId, forkIO)
+import           System.Environment    (getArgs, withArgs)
+import           System.Console.GetOpt (OptDescr(Option), ArgDescr(..), ArgOrder(..), usageInfo, getOpt)
+import           System.Exit           (exitSuccess)
+import           Test.HUnit            (runTestTT, Test, test)
 
 import Data.GraphViz (runGraphvizCanvas,GraphvizCommand(Dot),GraphvizCanvas(Xlib))
-import Bio.Phylogeny.PhyBin.CoreTypes
-         -- (NewickTree(..), PhyBinConfig(..), default_phybin_config, DefDecor, StandardDecor(..),
-         --  driver, parseNewick,
-         --  binthem, normalize, name_hack, annotateWLabLists, map_labels, set_dec,
-         --  drawNewickTree, dotNewickTree_debug, toLabel, fromLabel, Label,
-         --  run_tests)
-
-import Bio.Phylogeny.PhyBin
-         (NewickTree(..), PhyBinConfig(..), default_phybin_config, DefDecor, StandardDecor(..),
-          driver, parseNewick,
-          binthem, normalize, name_hack, annotateWLabLists, map_labels, set_dec,
-          drawNewickTree, dotNewickTree_debug, toLabel, fromLabel, Label
+import Bio.Phylogeny.PhyBin.CoreTypes 
+        ( NewickTree(..), PhyBinConfig(..), default_phybin_config, DefDecor, StandardDecor(..) ) 
+import Bio.Phylogeny.PhyBin 
+         ( driver, parseNewick,
+           binthem, normalize, annotateWLabLists, map_labels, set_dec,
+           drawNewickTree, dotNewickTree_debug, toLabel, fromLabel, Label,
+           unitTests
           )
 
 import Version
@@ -59,7 +49,8 @@ data Flag
 
   deriving (Show, Eq, Ord) -- ORD is really used.
 
-parseTabDelim str = 
+parseTabDelim :: String -> Flag
+parseTabDelim _str = 
   TabDelimited 8 9
     
 options :: [OptDescr Flag]
@@ -116,6 +107,7 @@ options =
 		  "\"<string> <taxaname>\", which are applied AFTER -s and -p."
      ]
 
+usage :: String
 usage = "\nUsage: phybin [OPTION...] files or directories...\n\n"++
 
         "PhyBin takes Newick tree files as input and produces, at minimum, files of the form\n"++
@@ -125,12 +117,19 @@ usage = "\nUsage: phybin [OPTION...] files or directories...\n\n"++
 	"If given a directory as input phybin will assume all contained files are Newick trees.\n\n"++
 
 	"\nOptions include:\n"
+
+defaultErr :: [String] -> t
 defaultErr errs = error $ "ERROR!\n" ++ (concat errs ++ usageInfo usage options)
 
 
+allUnitTests :: Test
 -- allUnitTests = unitTests ++
-allUnitTests = error "finish allunittests"
+allUnitTests = test 
+  [ Bio.Phylogeny.PhyBin.unitTests
+  ]
+--  Bio.Phylogeny.PhyBin.Parser.unitTests
 
+main :: IO ()
 main = 
   do argv <- getArgs 
 
@@ -144,7 +143,7 @@ main =
 	   Verbose -> return cfg { verbose= True } 
 	   Version -> do putStrLn$ "phybin version "++phybin_version; exitSuccess
 
-	   SelfTest -> do runTestTT allUnitTests; exitSuccess
+	   SelfTest -> do _ <- runTestTT allUnitTests; exitSuccess
                           
 	   Output s -> return cfg { output_dir= s }
 
@@ -185,10 +184,10 @@ view_graphs PBC{..} =
                 putStrLn$ "Drawing "++ file ++"...\n"
 		str <- B.readFile file
 		putStrLn$ "Parsed: " ++ (B.unpack str)
- 	        (chan, tr) <- drawNewickTree file $ 
-			      annotateWLabLists$ 
-			      map_labels name_hack $ 
-			      parseNewick file str
+ 	        (chan, _tr) <- drawNewickTree file $ 
+			       annotateWLabLists$ 
+			       map_labels name_hack $ 
+			       parseNewick file str
 	        return chan
 	      forM_ chans readChan 
 	      return ()
@@ -221,6 +220,7 @@ name_table_reader file =
         [one,two] -> [one,two]
         (one:rest) -> [one, unwords rest]
 
+temp :: IO ()
 temp = driver default_phybin_config{ num_taxa=7, inputs=["../datasets/test.tr"] }
 
 ----------------------------------------------------------------------------------------------------
@@ -228,34 +228,38 @@ temp = driver default_phybin_config{ num_taxa=7, inputs=["../datasets/test.tr"] 
 -- The below test exposed my normalization bug relating to "deriving Ord".
 -- I need to transform it into one or more proper unit tests.
 
+main_test :: IO ()
 main_test = 
  withArgs ["-w","~/newton_and_newton_local/datasets/yersinia/yersinia_trees/111.dnd","-m","../datasets/yersinia/name_table_hack_yersinia.txt"]
 	  main 
 
---a :: AnnotatedTree
--- annotateWLabLists$ 
-a :: NewickTree DefDecor
-a = set_dec (Nothing,1) $ 
+pa :: NewickTree DefDecor
+pa = set_dec (Nothing,1) $ 
     NTInterior () [NTInterior () [NTLeaf () "RE",NTInterior () [NTLeaf () "SD",NTLeaf () "SM"]],NTInterior () [NTLeaf () "BB",NTLeaf () "BJ"],NTInterior () [NTLeaf () "MB",NTLeaf () "ML"]]
 
-b :: NewickTree DefDecor
-b = set_dec (Nothing,1) $ 
+pb :: NewickTree DefDecor
+pb = set_dec (Nothing,1) $ 
     NTInterior () [NTInterior () [NTLeaf () "BB",NTLeaf () "BJ"],NTInterior () [NTLeaf () "MB",NTLeaf () "ML"],NTInterior () [NTLeaf () "RE",NTInterior () [NTLeaf () "SD",NTLeaf () "SM"]]]
 
-ls = [("a",a),("b",b)]
+ls1 :: [(String, NewickTree DefDecor)]
+ls1 = [("a",pa),("b",pb)]
 
 -- This is one:
-num_binned = M.size $ binthem ls
+num_binned :: Int
+num_binned = M.size $ binthem ls1
 
+a_ :: (String, NewickTree DefDecor)
 a_ = ("980.dnd",
       fmap (\x -> (Nothing,x)) $ 
       NTInterior 0.0 [NTInterior 5.697e-2 [NTLeaf 3.95e-2 "SM",NTLeaf 5.977e-2 "SD"],NTLeaf 0.13143 "RE",NTInterior 0.13899 [NTInterior 9.019e-2 [NTLeaf 0.11856 "BB",NTLeaf 0.13592 "BJ"],NTInterior 0.13194 [NTLeaf 0.19456 "MB",NTLeaf 0.16603 "ML"]]])
 
+b_ :: (String, NewickTree DefDecor)
 b_ = ("999.dnd",
       fmap (\x -> (Nothing,x)) $ 
       NTInterior 0.0 [NTInterior 6.527e-2 [NTInterior 0.13734 [NTLeaf 2.975e-2 "SM",NTLeaf 3.002e-2 "SD"],NTLeaf 0.18443 "RE"],NTInterior 6.621e-2 [NTLeaf 0.16184 "MB",NTLeaf 0.15233 "ML"],NTInterior 0.23143 [NTLeaf 9.192e-2 "BB",NTLeaf 0.10125 "BJ"]])
 
 -- But THIS is two:  ack!
+num2 :: Int
 num2 = M.size $ binthem [a_,b_]
 
 -- Here's the test that breaks things:
@@ -271,14 +275,22 @@ b_norm_ = NTInterior (0.0,7,["BB","BJ","MB","ML","RE","SD","SM"])
 b_norm :: NewickTree StandardDecor
 b_norm = fmap (\ (bl,w,ls) -> StandardDecor bl Nothing w ls) b_norm_
 
+d1 :: IO (Chan (), NewickTree StandardDecor)
 d1 = drawNewickTree "" a_norm
+
+d2 :: IO (Chan (), NewickTree StandardDecor)
 d2 = drawNewickTree "" b_norm
 
+d1_ :: IO ThreadId
 d1_ = forkIO $ do runGraphvizCanvas Dot (dotNewickTree_debug "" a_norm) Xlib; return ()
+                  
+d2_ :: IO ThreadId
 d2_ = forkIO $ do runGraphvizCanvas Dot (dotNewickTree_debug "" b_norm) Xlib; return ()
 
 
 -- | A of a tree with _____ weights attached to it:
+withBootstrap :: String
 withBootstrap = "((((A8F330_:0.01131438136322714984,(G0GWK2_:0.00568050636963043226,(Q92FV4_:0.00284163304504484121,((B0BVQ5_:0.00319487112504297311,A8GU65_:0.00000122123005994819)74:0.00279881991324161267,(C3PM27_:0.00560787769333294297,C4K2Z0_:0.00559642713265556899)15:0.00000122123005994819)4:0.00000122123005994819)56:0.00276851661606284868)60:0.00283144414216590342)76:0.00886304965525876697,(A8GQC0_:0.05449879836105625541,(A8F0B2_:0.04736199885985507840,Q4UJN9_:0.02648399728559588939)64:0.00905997055810744446)28:0.00323255855543533657)29:0.02237505187863457132,(Q1RGK5_:0.00000122123005994819,A8GYD7_:0.00000122123005994819)100:0.28299884298270094884)100:0.05776841634437222123,(Q9ZC84_:0.00000122123005994819,D5AYH5_:0.00000122123005994819)99:0.00951976341375833368,Q68VM9_:0.04408933524904214141);"
 
+withBootstrap2 :: String
 withBootstrap2 = "((((A8F330_:0.01131438136322714984,(G0GWK2_:0.00568050636963043226,(Q92FV4_:0.00284163304504484121,((B0BVQ5_:0.00319487112504297311,A8GU65_:0.00000122123005994819):0.00279881991324161267[74],(C3PM27_:0.00560787769333294297,C4K2Z0_:0.00559642713265556899):0.00000122123005994819[15]):0.00000122123005994819[4]):0.00276851661606284868[56]):0.00283144414216590342[60]):0.00886304965525876697[76],(A8GQC0_:0.05449879836105625541,(A8F0B2_:0.04736199885985507840,Q4UJN9_:0.02648399728559588939):0.00905997055810744446[64]):0.00323255855543533657[28]):0.02237505187863457132[29],(Q1RGK5_:0.00000122123005994819,A8GYD7_:0.00000122123005994819):0.28299884298270094884[100]):0.05776841634437222123[100],(Q9ZC84_:0.00000122123005994819,D5AYH5_:0.00000122123005994819):0.00951976341375833368[99],Q68VM9_:0.04408933524904214141);"
