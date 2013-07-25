b64b605580f8792c8fa7c312e90ee6b1183c8d28
Ryan Newton <rrnewton@gmail.com>
2013-07-25 06:55:42 -0400

    bump version

--- Version.hs
+++ Version.hs
@@ -4,7 +4,6 @@ module Version where
 
 -- TODO: automatically generate this:
 
-phybin_version = "0.2.5"
+phybin_version = "0.2.6"
                  -- NOTE: Remember to keep me in sync with the .cabal file
-
 
