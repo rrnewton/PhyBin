db5524513672083240173e3e10007034b199efa2
Ryan Newton <rrnewton@gmail.com>
2013-07-25 06:13:35 -0400

    WIP: The problem predated the prev rev.  Working around.  But now having a graphviz exception

--- Bio/Phylogeny/PhyBin/Binning.hs
+++ Bio/Phylogeny/PhyBin/Binning.hs
@@ -331,7 +331,8 @@ maybeInsert fn (Just x) ls = insertBy fn x ls
 annotateWLabLists :: NewickTree DefDecor -> AnnotatedTree
 annotateWLabLists tr = case tr of 
   NTLeaf (bs,bl) n      -> NTLeaf (StandardDecor bl bs 1 [n]) n
-  NTInterior (bs,bl) [] -> error "annotateWLabLists: internal invariant broken.  Shouldn't have NTInterior with null children."
+  NTInterior (bs,bl) [] -> NTInterior (StandardDecor bl bs 0 []) []
+  -- NTInterior (bs,bl) [] -> error "annotateWLabLists: internal invariant broken.  Shouldn't have NTInterior with null children."
   NTInterior (bs,bl) ls -> 
       let children = map annotateWLabLists ls in 
       NTInterior (StandardDecor bl bs
